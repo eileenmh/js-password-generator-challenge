ca98acf1a97369c059c3daa5af75c3b77577b6d9
Eileen Harvey <98053169+eileenmh@users.noreply.github.com>
2023-02-08 23:11:53 -0500

    create function that prompts for password character count

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
-// Assignment code here
-
-
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+function getCharacterCount() {
+  let characterCount = prompt ("How long do you want your password to be? Enter a number greater than or equal to 8 and less than or equal to 128.");
+  if (isNaN(characterCount)) {
+    return "The value you entered is not a number. Please try again."
+  } else {
+    return "The value is a number."
+  }
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+console.log(getCharacterCount())
+
+// ORIGINAL CODE
+// // Get references to the #generate element
+// var generateBtn = document.querySelector("#generate");
+
+// // Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
+
+//   passwordText.value = password;
+
+// }
+
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
