fddb6dc33438e3ec91c99730accbc409eec36445
Eileen Harvey <98053169+eileenmh@users.noreply.github.com>
2023-02-09 09:06:35 -0500

    add check for null or empty prompt response

--- script.js
+++ script.js
@@ -1,54 +1,26 @@
+//check if prompt response is empty
+//is it possible to stop the function if someone presses cancel?
+
 function getCharacterCount() {
   let characterCount = prompt ("How long do you want your password to be? Enter a whole number greater than or equal to 8 and less than or equal to 128.");
-  if (characterCount >= 8 && characterCount <= 128) {
-    console.log("Success!")
-  }
-  else {
-    if (isNaN(characterCount)) {
-      alert (characterCount + " is not a numeric value. Please try again.");
-    } 
-    else if (characterCount % 1 != 0) {
-      alert (characterCount + " is not a whole number. Please try again.");
-    }
-    else if (characterCount < 8) {
-      alert (characterCount + " is lower than the character count minimum of 8. Please try again.")
-    }
-    else {
-      alert (characterCount + " is higher than the character count maximum of 128. Please try again.")
-    }
-  getCharacterCount();
+    if (characterCount !== null && characterCount.length !== 0) {
+      if (isNaN(characterCount)) {
+        alert (characterCount + " is not a numeric value. Please try again.");
+      } 
+      else if (characterCount % 1 != 0) {
+        alert (characterCount + " is not a whole number. Please try again.");
+      }
+      else if (characterCount < 8) {
+        alert (characterCount + " is lower than the character count minimum of 8. Please try again.")
+      }
+      else {
+        alert (characterCount + " is higher than the character count maximum of 128. Please try again.");
+      }
+    getCharacterCount();
   }
 }
 getCharacterCount();
-//     else if ( ) { // check that it's greater than or equal to 8
 
-//     }
-//   //   else if ( ) { // check that it's less than or equal to 128
-//   //   getCharacterCount();
-//   //   }
-//   // }
-
-// }
-
-//   getCharacterCount();
-
-//   ( isNaN(characterCount) ) {
-//     alert ("The value you entered is not a numeric value. Please try again.");
-//     getCharacterCount();
-//   } 
-//   else if (characterCount % 1 != 0) {
-//     alert ("Please enter a whole number.");
-//     getCharacterCount();
-//   }
-//   else if ( characterCount < 8 ) {
-//     alert ()
-//     } else if ( characterCount > 128 ) {
-
-//     }
-//   }
-// }
-
-// console.log(getCharacterCount())
 
 // ORIGINAL CODE
 // // Get references to the #generate element
