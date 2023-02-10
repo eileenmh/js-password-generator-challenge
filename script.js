7d72099d5553b23dbb866e34a57d61c0bfe6926f
Eileen Harvey <98053169+eileenmh@users.noreply.github.com>
2023-02-09 21:42:57 -0500

    simplify character count function and add alert that explains next steps

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
-//check if prompt response is empty
-//is it possible to stop the function if someone presses cancel?
+//declare character types and their options
+const lowercaseCh = "abcdefghijklmnopqrstuvwxyz";
+const uppercaseCh = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const numberCh = "123456789";
+const specialCh = "!#$%&()*+-.,/:;<=>?@[]^_`{}|~";
 
+//let user choose a password length between 7 and 129 characters and validate the response
 function getCharacterCount() {
-  let characterCount = prompt ("How long do you want your password to be? Enter a whole number greater than or equal to 8 and less than or equal to 128.");
+  var characterCount = prompt ("How long do you want your password to be? Enter a number between 7 and 129. Numbers with decimal points will not be accepted.");
     if (characterCount !== null && characterCount.length !== 0) {
-      if (isNaN(characterCount)) {
-        alert (characterCount + " is not a numeric value. Please try again.");
+      if (isNaN(characterCount) || characterCount % 1 != 0) {
+        alert ("The value you entered is invalid. It must be a whole number between 7 and 129. Please try again.");
+        getCharacterCount();
       } 
-      else if (characterCount % 1 != 0) {
-        alert (characterCount + " is not a whole number. Please try again.");
-      }
-      else if (characterCount < 8) {
-        alert (characterCount + " is lower than the character count minimum of 8. Please try again.")
+      else if (characterCount < 8 || characterCount > 128) {
+        alert ("Your password must be more than 7 and less than 129 characters in length. Please try again.");
+        getCharacterCount();
       }
       else {
-        alert (characterCount + " is higher than the character count maximum of 128. Please try again.");
+        alert ("Your password will contain " + characterCount + " characters. You will now be prompted to select whether or not to include lowercase, uppercase, numeric, and/or special characters. You must select yes for at least one option. Click 'OK' to continue.");
       }
-    getCharacterCount();
   }
 }
-getCharacterCount();
-
-
+getCharacterCount(); 
 // ORIGINAL CODE
 // // Get references to the #generate element
 // var generateBtn = document.querySelector("#generate");
@@ -36,4 +36,4 @@ getCharacterCount();
 // }
 
 // // Add event listener to generate button
-/* generateBtn.addEventListener("click", writePassword) */
+/* generateBtn.addEventListener("click", writePassword)*/
